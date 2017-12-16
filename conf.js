0485f8351517327b08bca5db0fbd2c4cd323c258
Camille <camille@pc47.home>
2017-12-16 14:40:38 +0100

    rm conf

--- conf.js
+++
@@ -1,4 +0,0 @@
-const DBUSER = 'cammls'
-const DBPASS = 'coding'
-//exports.dbUrl = 'mongodb://'+DBUSER+':'+DBPASS+'@ees-dujxd.mongodb.net/test'
-exports.dbUrl = 'mongodb://cammls:coding@ees-shard-00-00-dujxd.mongodb.net:27017,ees-shard-00-01-dujxd.mongodb.net:27017,ees-shard-00-02-dujxd.mongodb.net:27017/admin?replicaSet=EES-shard-0&ssl=true'
